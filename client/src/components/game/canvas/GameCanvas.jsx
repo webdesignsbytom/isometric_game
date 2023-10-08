301204acf2c85a3d2539267fd07384e330e3d9ed
xtombrock <xtombrock1989@gmail.com>
2023-10-08 03:08:13 +0100

    working copy

--- client/src/components/game/canvas/GameCanvas.jsx
+++ client/src/components/game/canvas/GameCanvas.jsx
@@ -12,50 +12,46 @@ function GameCanvas() {
 
   const tileColumnOffset = 64; // pixels
   const tileRowOffset = 32; // pixels
-  const Xtiles = 10;
-  const Ytiles = 10;
+
+  let isProcessingClick = false;
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
+    // Set canvas height
     const width = window.innerWidth;
     const height = window.innerHeight;
     canvas.width = width;
     canvas.height = height;
 
-    const originX = width / 2 - (Xtiles * tileColumnOffset) / 2;
+    // Set tiles to build from center
+    const originX = width / 2 - (maxGridXLength * tileColumnOffset) / 2;
     const originY = height / 2;
-
-    console.log('ORIGINX', originX, originY);
 
     context.scale(1, 1);
     context.lineCap = 'round';
     context.strokeStyle = 'black';
-    context.lineWidth = 5;
+    context.lineWidth = 2;
 
     contextRef.current = context;
 
-    // Draw grid and initialize squares
-    // createCanvasGrid();
-    // updateAndRefreshCanvas();
     createTileGrid(originX, originY);
-    drawTileGrid()
+    drawTileGrid();
   }, []);
 
   const createTileGrid = (originX, originY) => {
     let id = 1;
-    let tilesArray = []
+    let tilesArray = [];
 
-    for (let Xi = Xtiles - 1; Xi >= 0; Xi--) {
-      for (let Yi = 0; Yi < Ytiles; Yi++) {
+    for (let Xi = maxGridXLength - 1; Xi >= 0; Xi--) {
+      for (let Yi = 0; Yi < maxGridYLength; Yi++) {
         const offX =
           (Xi * tileColumnOffset) / 2 + (Yi * tileColumnOffset) / 2 + originX;
         const offY =
           (Yi * tileRowOffset) / 2 - (Xi * tileRowOffset) / 2 + originY;
 
-        
-        const tile = new Tile(id, 1, offX, 1, offY, 1, 'red', 'green');
+        const tile = new Tile(id, offX, offY, 'red', 'green');
         tilesArray.push(tile);
 
         id++;
@@ -66,15 +62,94 @@ function GameCanvas() {
   };
 
   const drawTileGrid = () => {
-    console.log('AAAAAAAAAAAAAAAA');
     const context = contextRef.current;
 
     tilesRef.current.forEach((tile) => {
       tile.drawTile(context); // Pass whether the tile is hovered
     });
-  }
+  };
 
-  return <canvas ref={canvasRef} />;
+  const hoverOverTile = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+
+    const tiles = tilesRef.current;
+
+    // Initially, assume no tiles are hovered
+    tiles.forEach((tile) => {
+      tile.isHovered = false;
+    });
+
+    // Find the tile that the mouse is over and set it as hovered
+    const hoveredTile = tiles.find((tile) => {
+      // Convert mouse coordinates to isometric coordinates
+      const isoX = (offsetX - tile.offX) / tile.tileColumnOffset;
+      const isoY = (offsetY - tile.offY) / tile.tileRowOffset;
+
+      // Check if the mouse is within the bounds of the tile
+      return (
+        isoX >= 0 && isoY >= 0 && isoX <= 1 && isoY <= 1 && isoX + isoY <= 1
+      );
+    });
+
+    if (hoveredTile) {
+      hoveredTile.isHovered = true;
+    }
+
+    // Redraw the canvas to update the colors
+    drawTileGrid();
+  };
+
+  const clickOnTile = ({ nativeEvent }) => {
+    if (isProcessingClick) {
+      return; // Ignore additional click events while processing one
+    }
+    const tiles = tilesRef.current;
+
+    tiles.forEach((tile) => {
+      tile.isActive = false;
+    });
+
+    isProcessingClick = true;
+
+    const { offsetX, offsetY } = nativeEvent;
+
+    for (const tile of tilesRef.current) {
+
+      // Convert mouse coordinates to isometric coordinates
+      const isoX = (offsetX - tile.offX) / tile.tileColumnOffset;
+      const isoY = (offsetY - tile.offY) / tile.tileRowOffset;
+
+      // Check if the mouse is within the bounds of the tile
+      if (
+        isoX >= 0 &&
+        isoY >= 0 &&
+        isoX <= 1 &&
+        isoY <= 1 &&
+        isoX + isoY <= 1
+      ) {
+        // Mouse is clicking on this tile
+        console.log('Tile clicked');
+        tile.isActive = true;
+
+        // Break out of the loop to prevent further tiles from being clicked
+        break;
+      }
+    }
+
+    // Redraw the canvas to update the colors
+    drawTileGrid();
+
+    // Allow processing of the next click event
+    isProcessingClick = false;
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseMove={hoverOverTile}
+      onMouseDown={clickOnTile}
+    />
+  );
 }
 
 export default GameCanvas;
--- client/src/components/game/canvas/Tile.jsx
+++ client/src/components/game/canvas/Tile.jsx
@@ -1,11 +1,8 @@
 export class Tile {
-  constructor(id, xpos, offX, ypos, offY, size, fillColor, borderColor) {
+  constructor(id, offX, offY, fillColor, borderColor) {
     this.id = id;
-    this.xpos = xpos;
     this.offX = offX;
-    this.ypos = ypos;
     this.offY = offY;
-    this.size = size;
     this.fillColor = fillColor;
     this.borderColor = borderColor;
     this.isHovered = false;
@@ -16,39 +13,8 @@ export class Tile {
   }
 
   drawTile = (context) => {
-    console.log('OOOOOOOOOOOOOOOOOOOOOOO');
     // Draw tile interior
     context.beginPath();
-    context.fillStyle = 'yellow';
-    context.moveTo(this.offX, this.offY + this.tileRowOffset / 2);
-    context.lineTo(this.offX + this.tileColumnOffset / 2, this.offY);
-    context.lineTo(this.offX + this.tileColumnOffset, this.offY + this.tileRowOffset / 2);
-    context.lineTo(this.offX + this.tileColumnOffset / 2, this.offY + this.tileRowOffset);
-    context.closePath();
-    context.stroke();
-    context.fill();
-  };
-
-  draw(context) {
-    // Calculate isometric coordinates
-    console.log('This xpos is: ', this.xpos);
-    const offX =
-      this.xpos * this.tileColumnOffset - this.ypos * this.tileColumnOffset;
-    const offY =
-      this.xpos * this.tileRowOffset + this.ypos * this.tileRowOffset;
-
-    context.beginPath();
-
-    // Draw tile interior
-    context.moveTo(offX, offY + this.tileRowOffset / 2);
-    context.lineTo(offX + this.tileColumnOffset / 2, offY);
-    context.lineTo(offX + this.tileColumnOffset, offY + this.tileRowOffset / 2);
-    context.lineTo(offX + this.tileColumnOffset / 2, offY + this.tileRowOffset);
-    context.closePath();
-
-    // Set stroke and fill styles
-    context.strokeStyle = this.borderColor;
-    context.lineWidth = 2;
 
     if (this.isActive) {
       context.fillStyle = 'yellow';
@@ -58,8 +24,12 @@ export class Tile {
       context.fillStyle = this.fillColor;
     }
 
-    // Draw tile
+    context.moveTo(this.offX, this.offY + this.tileRowOffset / 2);
+    context.lineTo(this.offX + this.tileColumnOffset / 2, this.offY);
+    context.lineTo(this.offX + this.tileColumnOffset, this.offY + this.tileRowOffset / 2);
+    context.lineTo(this.offX + this.tileColumnOffset / 2, this.offY + this.tileRowOffset);
+    context.closePath();
     context.stroke();
     context.fill();
-  }
+  };
 }
