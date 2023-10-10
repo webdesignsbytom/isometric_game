82e6e5ed91af29fdbcd06ea7ea3d263c117e1016
xtombrock <xtombrock1989@gmail.com>
2023-10-10 23:49:58 +0100

    save

--- client/src/components/game/modals/BuyTileModal.jsx
+++ client/src/components/game/modals/BuyTileModal.jsx
@@ -1,21 +1,38 @@
 import React, { useContext } from 'react';
 // Context
 import { ToggleContext } from '../../../context/ToggleContext';
+import { startingTileCost } from '../../../utils/gameData/Constants';
 
 function BuyTileModal() {
   const { closeBuyTileModal } = useContext(ToggleContext);
 
   return (
-    <article className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 outline outline-1 outline-black bg-white rounded-xl h-[200px] w-[300px] p-2 grid grid-rows-rev'>
-      <div className='text-center'>Buy Tile</div>
-      <div className='grid items-center justify-center'>
-        <button
-          onClick={closeBuyTileModal}
-          className='bg-orange-800 px-2 py-1 rounded-xl active:scale-95 hover:brightness-95'
-        >
-          Close
-        </button>
-      </div>
+    <article className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 outline outline-1 outline-black bg-white rounded-xl h-[200px] w-[300px] p-2 grid'>
+      <section>
+        <div className='text-center'>Buy Tile</div>
+        <div className='text-center text-xl font-bold'>
+          <span>🪙 </span>
+          {startingTileCost}
+        </div>
+      </section>
+      <section className='grid grid-cols-2 gap-1'>
+        <div className='grid items-center justify-center'>
+          <button
+            onClick={closeBuyTileModal}
+            className='bg-orange-800 px-2 py-1 rounded-xl active:scale-95 hover:brightness-95'
+          >
+            Buy
+          </button>
+        </div>
+        <div className='grid items-center justify-center'>
+          <button
+            onClick={closeBuyTileModal}
+            className='bg-orange-800 px-2 py-1 rounded-xl active:scale-95 hover:brightness-95'
+          >
+            Close
+          </button>
+        </div>
+      </section>
     </article>
   );
 }
--- client/src/utils/gameData/Constants.jsx
+++ client/src/utils/gameData/Constants.jsx
@@ -2,3 +2,4 @@ export const maxGridXAxisLength = 15;
 export const maxGridYAxisLength = 15;
 export const unownedTileColourHex = '#e0b33650';
 export const ownedTileColourHex = '#e0b336';
+export const startingTileCost = 10;
