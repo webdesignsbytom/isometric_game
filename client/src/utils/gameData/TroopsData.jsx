2375cfbcb0e6113e4d48d3d57a252b3a4b557665
xtombrock <xtombrock1989@gmail.com>
2023-10-12 01:13:43 +0100

    save

---
+++ client/src/utils/gameData/TroopsData.jsx
@@ -0,0 +1,15 @@
+export const TroopsAvailableArray = [
+  {
+    id: 1,
+    name: 'axe_warrior',
+    title: 'Axe Warrior',
+    cost: 50,
+    timeToTrain: 10000,
+    trainingPeriod: '10 Seconds',
+    strength: 10,
+    defense: 10,
+    health: 100,
+    imageUrl: 'https://',
+    description: 'Im an axe warrior',
+  },
+];
