d660e803de9b9733cae1f97c1ce4e905e0f644e9
xtombrock <xtombrock1989@gmail.com>
2023-10-16 19:26:27 +0100

    save

--- client/src/components/game/menus/CityMenu/CityMenuBar.jsx
+++ client/src/components/game/menus/CityMenu/CityMenuBar.jsx
@@ -3,10 +3,19 @@ import React, { useContext } from 'react';
 import { PlayerContext } from '../../../../context/PlayerContext';
 
 function CityMenuBar() {
-    const { player } = useContext(PlayerContext)
+  const { player } = useContext(PlayerContext);
+
   return (
     <section className='absolute top-1 left-1/2 transform -translate-x-1/2 outline outline-1 outline-black bg-white rounded-xl h- p-2 grid grid-rows-rev'>
-      CityMenuBar
+      <article>
+        <section>
+          <div className='text-center'><span>{player.cityData.cityName}</span></div>
+        </section>
+        <section>
+          <div>1</div>
+          <div>1</div>
+        </section>
+      </article>
     </section>
   );
 }
